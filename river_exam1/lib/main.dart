4559e9716ee37c09f437b782299f9d241fd84df6
yussifm <yussifm13@gmail.com>
2022-12-12 12:55:36 +0000

    river exam 1 done

--- river_exam1/lib/main.dart
+++ river_exam1/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
